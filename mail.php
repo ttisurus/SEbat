86393e864c82656562276463827258a38cd0ee53
ttisurus <argdan90@gmail.com>
2020-09-09 15:31:33 -0300

    modificacion del logo tanto en nav como en footer. modificacion de imagenes en las diferentes secciones. Cambio de informacion en las secciones correspondientes. Creacion de formulario de validacion en javascript para el form de contacto. Creacion del codigo php para conectar el formulario con un envio por mail, todavia sin funcionar. Agregado de fuente correspondiente al tipo usado en el logo, para aplicar en la navbar

---
+++ mail.php
@@ -0,0 +1,36 @@
+<?php
+/* $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+header('Content-Type: application/json');
+if ($name === ''){
+  print json_encode(array('message' => 'Completar con su nombre', 'code' => 0));
+  exit();
+}
+if ($email === ''){
+  print json_encode(array('message' => 'Completar con un email valido', 'code' => 0));
+  exit();
+} else {
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+  print json_encode(array('message' => 'Completar con un email valido', 'code' => 0));
+  exit();
+  }
+}
+if ($subject === ''){
+  print json_encode(array('message' => 'Complete con el motivo de su mensaje', 'code' => 0));
+  exit();
+  }
+if ($message === ''){
+  print json_encode(array('message' => 'Completar con su mensaje', 'code' => 0));
+  exit();
+}
+
+
+$content="From: $name \nEmail: $email \nMessage: $message";
+$recipient = "argdan90@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+print json_encode(array('message' => 'Su mensaje ha sido enviado. Muchas gracias!', 'code' => 1));
+exit(); */
+?>
