27c8f2e286cfb85320757ff9719936c45e0f916f
ttisurus <argdan90@gmail.com>
2020-09-11 02:14:07 -0300

    modificacion de tipo de dato enviado a traves de JSON en form de validacion JS. Modificacion de las condiciones de envio de formulario en archivo php, haciendo una reduccion de codigo y modificando la forma de obtener los datos desde el form html

--- js/validate-form.js
+++ js/validate-form.js
@@ -1,21 +1,24 @@
-function validateForm() {
+function validateForm(e) {
+    e.preventDefault();
     document.getElementById('status').innerHTML = "Sending...";
     formData = {
         'name': $('input[name=name]').val(),
         'email': $('input[name=email]').val(),
-        'subject'  : $('input[name=subject]').val(),
+        'subject': $('input[name=subject]').val(),
         'message': $('textarea[name=message]').val()
     };
 
     $.ajax({
         url: "mail.php",
         type: "POST",
+        dataType: "JSON",
         data: formData,
         success: function (data, textStatus, jqXHR) {
 
-            $('#status').text(data.message);
-            if (data.code) //If mail was sent successfully, reset the form.
-                $('#contact-form').closest('form').find("input[type=text], textarea").val("");
+            document.getElementById('status').innerHTML = data.message;
+            if (data.code) {
+                $('#contact-form').find("input[type=text], textarea").val("");
+            }
         },
         error: function (jqXHR, textStatus, errorThrown) {
             $('#status').text(jqXHR);
--- mail.php
+++ mail.php
@@ -4,27 +4,26 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-header('Content-Type: application/json');
+function print_message($error_message, $code = 0) {
+  header('Content-type', 'application/json');
+  print json_encode(['message' => $error_message, 'code' => $code]);
+  exit();
+}
+
 if ($name === ''){
-  print json_encode(array('message' => 'Completar con su nombre', 'code' => 0));
-  exit();
+  print_message('Completar con su nombre');
 }
-if ($email === ''){
-  print json_encode(array('message' => 'Completar con un email valido', 'code' => 0));
-  exit();
-} else {
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-  print json_encode(array('message' => 'Completar con un email valido', 'code' => 0));
-  exit();
-  }
+
+if ($email === '' || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  print_message('Completar con un email valido');
 }
-if ($subject === ''){
-  print json_encode(array('message' => 'Complete con el motivo de su mensaje', 'code' => 0));
-  exit();
-  }
-if ($message === ''){
-  print json_encode(array('message' => 'Completar con su mensaje', 'code' => 0));
-  exit();
+
+if ($subject === '') {
+  print_message('Complete con el motivo de su mensaje');
+}
+
+if ($message === '') {
+  print_message('Completar con su mensaje');
 }
 
 
@@ -32,10 +31,6 @@ $content="From: $name \nEmail: $email \nMessage: $message";
 $recipient = "argdan90@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
-print json_encode(array('message' => 'Su mensaje ha sido enviado. Muchas gracias!', 'code' => 1));
-/* if($email) {
-  echo "<script>alert('Su mensaje a sido enviado')</script>";
-  echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
-} */
+print_message('Su mensaje ha sido enviado. Muchas gracias!', 1);
 exit();
 ?>
